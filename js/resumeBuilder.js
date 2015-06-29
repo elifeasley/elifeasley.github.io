ac27f21ad3f830a908ab532e28273d2518755425
Eliana Feasley <eliana@khanacademy.org>
2015-06-29 06:36:00 +0000

    Render data to about page
    Yeah, in like 20 different styles.
    You wanna take this outside? Ready to go?

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,56 +1,3 @@
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
-
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
-
-var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
-
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
-
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
-
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
-
-var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
-
-
-
-var formattedName = HTMLheaderName.replace("%data%","eli feasley");
-var formattedRole = HTMLheaderRole.replace("%data%", "data scientist");
-
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedRole);
 var bio =  {
     "name": "eli feasley",
     "role": "\"data\" \"scientist\"",
@@ -71,9 +18,25 @@ var bio =  {
     "biopic": "https://drive.google.com/file/d/1oSmMed5nJVm_rjDstOdyd5rTvr4Nif_SlQ/view?usp=sharing"
 };
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile)
+bio.display = function() {
+    var formattedName = HTMLheaderName.replace("%data%","eli feasley");
+    var formattedRole = HTMLheaderRole.replace("%data%", "data scientist");
+    
+    $("#header").prepend(formattedName);
+    $("#header").prepend(formattedRole);
 
-$("#main").append(formattedMobile);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    var formattedGithub = HTMLmobile.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    
+    $("#topContacts").append(formattedMobile);
+    $("#topContacts").append(formattedEmail);
+    $("#topContacts").append(formattedTwitter);
+    $("#topContacts").append(formattedGithub);
+    $("#topContacts").append(formattedLocation);
+};
 
 
 var education = {
@@ -106,7 +69,7 @@ var education = {
 //          url: string
 //     display: function taking no parameters
 // work contains
-var jobs = {
+var work = {
     "jobs": [
         {
             "employer": "khan academy",
@@ -129,17 +92,20 @@ var jobs = {
     ]
 };
 
-for (var job in jobs["jobs"]) {
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", jobs.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", jobs.jobs[job].title);
-    var formattedLocation = HTMLworkLocation.replace("%data%", jobs.jobs[job].location);
-    var formattedDescription = HTMLworkDescription.replace("%data%", jobs.jobs[job].description);
-    
-    var employerTitle = formattedEmployer + formattedTitle + formattedLocation + formattedDescription;
-    $(".work-entry:last").append(employerTitle);
-
+work.display = function() {
+    for (var job in work["jobs"]) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        
+        var employerTitle = formattedEmployer + formattedTitle + formattedLocation + formattedDescription;
+        $(".work-entry:last").append(employerTitle);
+    }
 }
+
+work.display();
 //     jobs: array of objects with
 //          employer: string 
 //          title: string 
@@ -163,6 +129,21 @@ var projects = {
         }
         ]
 };
+
+projects.display = function() {
+    for (var job in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[job].title);
+        var formattedDate = HTMLprojectDates.replace("%data%", projects.projects[job].date);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[job].description);
+        
+        var employerTitle = formattedTitle + formattedDate + formattedDescription;
+        $(".project-entry:last").append(employerTitle);
+    }
+}
+projects.display()
+bio.display();
+$("#mapDiv").append(googleMap);
 // projects contains:
 
 //     projects: array of objects with
